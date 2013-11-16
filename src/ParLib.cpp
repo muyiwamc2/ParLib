7e53307706249114d2f7343b3bcaa1b165469e4f
Olumuyiwa Oluwasanmi <muyiwamc2@gmail.com>
2013-11-16 01:26:09 -0800

    implemented accumulate!

--- src/ParLib.cpp
+++ src/ParLib.cpp
@@ -10,18 +10,16 @@
 #include <iostream>
 #include "ParLib.h"
 
-
-
-
 int main() {
 	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
-	 std::vector<int> one(1000);
-	   std::iota(one.begin(),one.end(),1);
-	   std::vector<double> two(1000);
-	   std::cout<<"starting transform"<<std::endl;
-	   parallel::transform(one.begin(),one.end(),two.begin(),[](int k)->double{ return 5.0+ 3.141592 *static_cast<double>(k);});
-	   parallel::for_each(two.begin(),two.end(),[](double k){ std::cout<< k<<std::endl;});
-
+	std::vector<int> one(1000);
+	std::vector<double> two(1000);
+	std::iota(one.begin(),one.end(), 1);
+	std::cout << "starting transform" << two.size() << std::endl;
+	parallel::transform(one.begin(), one.end(), two.begin(),
+			[](int k)->double {return 3.14159*static_cast<double>(k);});
+	std::for_each(one.begin(), one.end(), [](double k) {std::cout<< k<<std::endl;});
+	std::cout<< " The sum is:"<< parallel::accumulate(one.begin(),one.end(),0)<<std::endl;
 
 	return 0;
 }
--- src/ParLib.h
+++ src/ParLib.h
@@ -8,7 +8,6 @@
 #ifndef PARLIB_H_
 #define PARLIB_H_
 
-
 /*
  * ParLib.h
  *
@@ -26,171 +25,245 @@
 #include<iostream>
 
 namespace parallel {
-    enum ThreadTypes { standard,async};
-    template< class InputIt,  unsigned int blksz=25,  ThreadTypes tT=ThreadTypes::standard>
-	class LaunchPolicies{
-	public:
-		ThreadTypes tTypes;
-		unsigned long  length ;
-		unsigned long hardware_threads=std::thread::hardware_concurrency();
-		unsigned long min_per_thread;
-		unsigned long max_threads ;
-		unsigned long num_threads ;
-		unsigned long block_size ;
-		void SetLaunchPolicies(InputIt beg, InputIt end){
-			tTypes=tT;
-			length =std::distance(beg,end);
-			hardware_threads=std::thread::hardware_concurrency();
-			min_per_thread =blksz;
-			max_threads=(length + min_per_thread)/min_per_thread; ;
-			num_threads = std::min(hardware_threads !=0?hardware_threads:2,max_threads);
-			block_size = length/num_threads ;
-		}
+	enum class ThreadTypes {
+		standard, async
 	};
-    template<class InputIt>
-   	class LaunchPolicies<InputIt, 25 ,ThreadTypes::standard>{
-   	public:
-   		ThreadTypes tTypes;
-   		unsigned long  length ;
-   		unsigned long hardware_threads;
-   		unsigned long min_per_thread;
-   		unsigned long max_threads ;
-   		unsigned long num_threads ;
-   		unsigned long block_size ;
-   		void SetLaunchPolicies(InputIt beg, InputIt end){
-   			tTypes=ThreadTypes::standard;
-   			length =std::distance(beg,end);
-   			hardware_threads=std::thread::hardware_concurrency();
-   			min_per_thread =25;
-   			max_threads=(length + min_per_thread)/min_per_thread; ;
-   			num_threads = std::min(hardware_threads !=0?hardware_threads:2,max_threads);
-   			block_size = length/num_threads ;
-   		}
-   	};
+	template<class InputIt, unsigned int blksz = 25, ThreadTypes tT = ThreadTypes::standard>
+	class LaunchPolicies {
+		public:
+			ThreadTypes tTypes;
+			unsigned long length;
+			unsigned long hardware_threads = std::thread::hardware_concurrency();
+			unsigned long min_per_thread;
+			unsigned long max_threads;
+			unsigned long num_threads;
+			unsigned long block_size;
+			void SetLaunchPolicies(InputIt beg, InputIt end) {
+				tTypes = tT;
+				length = std::distance(beg, end);
+				hardware_threads = std::thread::hardware_concurrency();
+				min_per_thread = blksz;
+				max_threads = (length + min_per_thread) / min_per_thread;
+				;
+				num_threads = std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
+				block_size = length / num_threads;
+			}
+	};
+	template<class InputIt>
+	class LaunchPolicies<InputIt, 25, ThreadTypes::standard> {
+		public:
+			ThreadTypes tTypes;
+			unsigned long length;
+			unsigned long hardware_threads;
+			unsigned long min_per_thread;
+			unsigned long max_threads;
+			unsigned long num_threads;
+			unsigned long block_size;
+			void SetLaunchPolicies(InputIt beg, InputIt end) {
+				tTypes = ThreadTypes::standard;
+				length = std::distance(beg, end);
+				hardware_threads = std::thread::hardware_concurrency();
+				min_per_thread = 25;
+				max_threads = (length + min_per_thread) / min_per_thread;
+				;
+				num_threads = std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
+				block_size = length / num_threads;
+			}
+	};
 
-    template <class InputIt, class UnaryFunction >
-    struct foreach_block{
-       void operator ()(InputIt beg, InputIt end,UnaryFunction  f){
-           std::for_each(beg,end,f);
-       }
-    };
+	template<class InputIt, class UnaryFunction>
+	struct foreach_block {
+			void operator ()(InputIt beg, InputIt end, UnaryFunction f) {
+				std::for_each(beg, end, f);
+			}
+	};
+
+	template<class InputIt, class UnaryFunction, class Tpolicy = LaunchPolicies<InputIt> >
+	UnaryFunction for_each(InputIt beg, InputIt end, UnaryFunction f) {
+		Tpolicy Tp;
+		Tp.SetLaunchPolicies(beg, end);
+		if(!Tp.length)
+			return f;
+
+		std::vector < std::thread > threads(Tp.num_threads - 1);
+		InputIt block_start = beg;
+		InputIt block_end = beg;
+		for(int i; i < (Tp.num_threads - 1); i++) {
+
+			std::advance(block_end, Tp.block_size);
+			threads[i] = std::thread(foreach_block<InputIt, UnaryFunction>(), block_start,
+					block_end, f);
+
+			block_start = block_end;
+		}
+		foreach_block<InputIt, UnaryFunction>()(block_start, end, f);
+		std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
+
+		return f;
+	}
+
+	template<class InputIt, class OutputIt, class UnaryOperator>
+	struct transform_block {
+			void operator()(InputIt first1, InputIt last1, OutputIt result, UnaryOperator op) {
+				std::transform(first1, last1, result, op);
+
+			}
+	};
+	template<class InputIt, class InputIt2, class OutputIt, class BinaryOperator>
+	struct transform_block2 {
+			void operator()(InputIt first1, InputIt last1, InputIt2 first2, OutputIt result,
+					BinaryOperator op) {
+				std::transform(first1, last1, first2, result, op);
+
+			}
+	};
+
+	template<class InputIt, class OutputIt, class UnaryOperator, class Tpolicy = LaunchPolicies<
+			InputIt>>
+	OutputIt transform(InputIt beg, InputIt end, OutputIt result, UnaryOperator op) {
+		Tpolicy Tp;
+		Tp.SetLaunchPolicies(beg, end);
+		if(!Tp.length)
+			return result;
+
+		std::vector < std::thread > threads(Tp.num_threads - 1);
+		InputIt block_start = beg;
+		InputIt block_end = beg;
+		InputIt last = end;
+
+		OutputIt outblock_start = result;
+		for(int i = 0; i < (Tp.num_threads - 1); i++) {
+
+			std::advance(block_end, Tp.block_size);
+			threads[i] = std::thread(transform_block<InputIt, OutputIt, UnaryOperator>(),
+					block_start, block_end, outblock_start, op);
+			block_start = block_end;
+			std::advance(outblock_start, Tp.block_size);
+		}
+		transform_block<InputIt, OutputIt, UnaryOperator>()(block_start, last, outblock_start,
+				op);
+		std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
+
+		return result;
+	}
+
+	template<class InputIt, class InputIt2, class OutputIt, class BinaryOperator,
+			class Tpolicy = LaunchPolicies<InputIt>>
+	OutputIt transform(InputIt beg1, InputIt end1, InputIt2 beg2, OutputIt result,
+			BinaryOperator op) {
+		Tpolicy Tp;
+		Tp.SetLaunchPolicies(beg1, end1);
+		if(!Tp.length)
+			return result;
+
+		std::vector < std::thread > threads(Tp.num_threads - 1);
+		InputIt block_start1 = beg1;
+		InputIt2 block_start2 = beg2;
+		InputIt block_end1 = beg1;
+		InputIt last1 =end1;
+
+
+		OutputIt outblock_start = result;
+		for(int i = 0; i < (Tp.num_threads - 1); i++) {
+
+			std::advance(block_end1, Tp.block_size);
+			threads[i] = std::thread(
+					transform_block2<InputIt, InputIt2, OutputIt, BinaryOperator>(), block_start1,
+					block_end1, block_start2, outblock_start, op);
+
+			block_start1 = block_end1;
+			std::advance(block_start2, Tp.block_size);
+			std::advance(outblock_start, Tp.block_size);
+		}
+		transform_block2<InputIt, InputIt2, OutputIt, BinaryOperator>()(block_start2, last1,
+				block_start2, outblock_start, op);
+		std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
+
+		return result;
+	}
+
+	template<class InputIt, class T>
+		struct accumulate_block {
+				T operator()(InputIt beg, InputIt end, T &ret) {
+
+					ret =std::accumulate(beg, end, ret);
+					return ret;
+
+				}
+		};
+
+	template<class InputIt, class T, class BinaryOperator>
+			struct accumulate_block2 {
+					T operator()(InputIt beg, InputIt end, T &ret, BinaryOperator op) {
+
+						ret= std::accumulate(beg, end, ret,op);
+						return ret;
+
+					}
+			};
 
 
 
-       template<class InputIt, class UnaryFunction, class Tpolicy =LaunchPolicies<InputIt> >
-       UnaryFunction for_each(InputIt beg, InputIt end, UnaryFunction f){
-    	   Tpolicy Tp;
-    	   Tp.SetLaunchPolicies(beg,end);
-           //Tp.length = std::distance(beg,end);
-           if(!Tp.length)return f;
+	template<class InputIt, class T, class Tpolicy = LaunchPolicies<
+				InputIt>>
+		T accumulate(InputIt beg, InputIt end, T init) {
+			Tpolicy Tp;
+			Tp.SetLaunchPolicies(beg, end);
+			if(!Tp.length)
+				return init;
+
+			std::vector < std::thread > threads(Tp.num_threads - 1);
+			std::vector <T> output(Tp.num_threads);
+			InputIt block_start = beg;
+			InputIt block_end = beg;
+			InputIt last = end;
 
 
-           std::vector<std::thread> threads(Tp.num_threads-1);
-           InputIt block_start =beg;
-           InputIt block_end=beg ;
-           for(int i;i<(Tp.num_threads-1);i++){
+			for(int i = 0; i < (Tp.num_threads - 1); i++) {
 
-             std::advance(block_end,Tp.block_size);
-             threads[i]= std::thread(foreach_block<InputIt,UnaryFunction>(),block_start,block_end,f) ;
+				std::advance(block_end, Tp.block_size);
+				threads[i] = std::thread(accumulate_block<InputIt, T>(),
+						block_start, block_end, std::ref(output[i]));
+				block_start = block_end;
+			}
+			accumulate_block<InputIt, T>()(block_start, last, std::ref(output[Tp.num_threads-1]));
+			std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
 
-             block_start=block_end;
-           }
-           foreach_block<InputIt,UnaryFunction>()(block_start,end,f);
-           std::for_each(threads.begin(),threads.end(),std::mem_fn(&std::thread::join));
 
-           return f;
-       }
+			return std::accumulate(output.begin(),output.end(),init);
+		}
 
-       template <class InputIt, class OutputIt, class UnaryOperator>
-           struct transform_block {
-               void operator() (InputIt first1, InputIt last1,OutputIt result, UnaryOperator op)
-           	{
-           		std::transform(first1,last1,result, op);
+	template<class InputIt, class T, class BinaryOperator, class Tpolicy = LaunchPolicies<
+					InputIt>>
+			T accumulate(InputIt beg, InputIt end, T init, BinaryOperator op) {
+				Tpolicy Tp;
+				Tp.SetLaunchPolicies(beg, end);
+				if(!Tp.length)
+					return init;
 
-           	}
-           };
-           template <class InputIt, class InputIt2, class OutputIt, class BinaryOperator>
-               struct transform_block2 {
-               	void operator() (InputIt first1, InputIt last1,InputIt2 first2,
-                                       OutputIt result, BinaryOperator op)
-               	{
-               		std::transform(first1,last1, first2, result, op);
+				std::vector < std::thread > threads(Tp.num_threads - 1);
+				std::vector <T> output(Tp.num_threads);
+				InputIt block_start = beg;
+				InputIt block_end = beg;
+				InputIt last = end;
 
-               	}
-               };
 
-           template <class InputIt, class OutputIt, class UnaryOperator,class Tpolicy =LaunchPolicies<InputIt>>
-           OutputIt transform(InputIt beg, InputIt end,OutputIt result,UnaryOperator op){
-        	              Tpolicy Tp;
-        	              Tp.SetLaunchPolicies(beg,end);
-        	              if(!Tp.length)return result;
+				for(int i = 0; i < (Tp.num_threads - 1); i++) {
 
-        	              std::vector<std::thread> threads(Tp.num_threads-1);
-        	              InputIt block_start =beg;
-        	              InputIt block_end=beg ;
+					std::advance(block_end, Tp.block_size);
+					threads[i] = std::thread(accumulate_block2<InputIt, T, BinaryOperator>(),
+							block_start, block_end, std::ref(output[i]),op);
+					block_start = block_end;
+				}
+				accumulate_block2<InputIt, T, BinaryOperator>()(block_start, last, std::ref(output[Tp.num_threads-1]),op);
+				std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
 
-        	              OutputIt outblock_start=result;
-        	              for(int i=0;i<(Tp.num_threads-1);i++){
 
-        	                std::advance(block_end,Tp.block_size);
-        	                threads[i]= std::thread(transform_block<InputIt,OutputIt,UnaryOperator>(),
-        	                		block_start,block_end,outblock_start,op) ;
+				return std::accumulate(output.begin(),output.end(),init,op);
+			}
 
-        	                block_start=block_end;
-        	                std::advance(outblock_start,Tp.block_size);
-        	              }
-        	              transform_block<InputIt,OutputIt,UnaryOperator>()(block_start,block_end,outblock_start,op);
-        	              std::for_each(threads.begin(),threads.end(),std::mem_fn(&std::thread::join));
-
-        	              return result;
-           }
-
-           template <class InputIt, class InputIt2, class OutputIt, class BinaryOperator,class Tpolicy =LaunchPolicies<InputIt>>
-                      OutputIt transform(InputIt beg1, InputIt end1, InputIt2 beg2, OutputIt result,BinaryOperator op){
-        	   	   	   	   	   	   Tpolicy Tp;
-        	           	           Tp.SetLaunchPolicies(beg1,end1);
-                   	              if(!Tp.length)return result;
-
-                   	              std::vector<std::thread> threads(Tp.num_threads-1);
-                   	              InputIt block_start1 =beg1;
-                   	              InputIt2 block_start2=beg2;
-                   	              InputIt block_end1=beg1 ;
-
-                   	              OutputIt outblock_start=result;
-                   	              for(int i=0;i<(Tp.num_threads-1);i++){
-
-                   	                std::advance(block_end1,Tp.block_size);
-                   	                threads[i]= std::thread(transform_block2<InputIt,InputIt2,OutputIt,BinaryOperator>(),
-                   	                		block_start1,block_end1,block_start2,outblock_start,op) ;
-
-                   	                block_start1=block_end1;
-                   	                std::advance(block_start2,Tp.block_size);
-                   	                std::advance(outblock_start,Tp.block_size);
-                   	              }
-                   	              transform_block2<InputIt,InputIt2,OutputIt,BinaryOperator>()(block_start2,block_end1, block_start2,outblock_start,op);
-                   	              std::for_each(threads.begin(),threads.end(),std::mem_fn(&std::thread::join));
-
-                   	              return result;
-                      }
 
 }
-
-
-// _end pointer point not to the last element, but one past and never access it.
-/*
-template< class _Type >
-inline void merge_ptr( const _Type* a_start, const _Type* a_end, const _Type* b_start, const _Type* b_end, _Type* dst )
-{
-    while( a_start < a_end && b_start < b_end ) {
-        if ( *a_start <= *b_start )  *dst++ = *a_start++;    // if elements are equal, then a[] element is output
-        else                    	*dst++ = *b_start++;
-    }
-    while( a_start < a_end ) *dst++ = *a_start++;
-    while( b_start < b_end ) *dst++ = *b_start++;
-}
-*/
-
-
 
 
 #endif /* PARLIB_H_ */
